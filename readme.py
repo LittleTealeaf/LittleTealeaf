a04b2fa65930725ea582d20b73cbc170b8e194b9
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-09 12:50:18 -0500

    misc additionsg

--- readme.py
+++ readme.py
@@ -1,4 +1,8 @@
 import json
+import requests
+
+def get(url):
+    return requests.get(url).json()
 
 def get_data():
     f = open("./resources/content.json")
@@ -12,6 +16,11 @@ def about_me(data):
 
 
 data = get_data()
+followers = get("https://api.github.com/users/LittleTealeaf/followers")
+repositories = get("https://api.github.com/users/LittleTealeaf/repos")
+#   https://api.github.com/repos/octocat/hello-world/commits
+
+
 
 print(data["intro"])
 about_me(data)
