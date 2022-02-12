2fadf881bf53569b82ea007ce41b8bf65bd27852
Thomas Kwashnak <shintaomonk@gmail.com>
2022-02-11 23:26:30 -0500

    Updated Recent Activity

--- python/readme.py
+++ python/readme.py
@@ -3,6 +3,8 @@ import requests
 from html import *
 from icons import *
 
+username = "LittleTealeaf"
+
 # Output is made using the "print()" method, as the output is piped into a file using the > operator
 
 f_content = open("./resources/content.json")
@@ -12,6 +14,7 @@ f_content.close()
 f_events = open("./tmp/events.json")
 events = json.load(f_events)
 f_events.close()
+# events = requests.get(f"https://api.github.com/users/{username}/events").json()
 
 def recent_repositories():
     print(header("Recent Activity",2))
@@ -31,10 +34,11 @@ def recent_repositories():
         revents = []
         for event in repo['events']:
             if event['type'] == 'PushEvent':
+                branch = event['payload']['ref'].partition('heads/')[-1]
                 for commit in event['payload']['commits']:
                     commit_link = link(f"#{commit['sha'][:7]}",f"https://www.github.com/{repo_name}/commit/{commit['sha']}")
                     message = commit['message'].partition('\n')[0]
-                    revents.append(f"{commit_link} {message}")
+                    revents.append(f"<code>{branch}</code> {commit_link}  {message}")
 
         output.append(details(link(repo_name,f"https://www.github.com/{repo_name}"),hlist(revents)))
 
