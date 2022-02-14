905e2a6b41d820bb232b47096746029da8baa201
Thomas Kwashnak <shintaomonk@gmail.com>
2022-02-13 21:12:17 -0500

    Recent Repositories now contains commits

--- python/readme.py
+++ python/readme.py
@@ -30,32 +30,60 @@ def print_json_file(name,jsonObject):
     jsonBlock = json_block(jsonObject)
     return f"{fileName}\n\n{jsonBlock}"
 
-def print_recent_repositories(eventsJson):
-    repos = []
-    for event in eventsJson:
+def print_recent_repositories(events):
+    # repos = []
+    # for event in eventsJson:
+    #     if event['repo']['url'] not in repos:
+    #         repos.append(event['repo']['url'])
+    # repoString = []
+    # for repo in repos[:5]:
+    #     api = api_github(repo)
+    #     link = html_link(api['full_name'],api['html_url'])
+    #     repoString.append(f"{link} - {api['description']}")
+    # header = html_header("Recent Repositories",3)
+    # string = html_list(repoString)
+    # return f"{header}\n{string}"
+    repos = {}
+    for event in events:
         if event['repo']['url'] not in repos:
-            repos.append(event['repo']['url'])
-    repoString = []
-    for repo in repos[:5]:
-        api = api_github(repo)
-        link = html_link(api['full_name'],api['html_url'])
-        repoString.append(f"{link} - {api['description']}")
-    header = html_header("Recent Repositories",3)
-    string = html_list(repoString)
-    return f"{header}\n{string}"
+            repos[event['repo']['url']] = [event]
+        else:
+            repos[event['repo']['url']].append(event)
+    reposOut = []
+    for repo in repos:
+        repoApi = api_github(repo)
+        repoLink = html_link(repoApi['full_name'],repoApi['html_url'])
+        description = repoApi['description']
+        events = []
+        for event in repos[repo]:
+            if event['type'] == 'PushEvent':
+                branch = event['payload']['ref'].split('/')[2]
+                for commit in event['payload']['commits']:
+                    sha = commit['sha'][:7]
+                    commitApi = api_github(commit['url'])
+                    commitLink = html_link(f"#{sha}",commitApi['html_url'])
+                    message = commit['message'].partition('\n')[0]
+                    events.append(f"<code>{branch}</code> {commitLink} - {message}")
+            elif event['type'] == 'ForkEvent':
+                forkLink = html_link(event['payload']['forkee']['full_name'],event['payload']['forkee']['html_url'])
+                events.append(f"Created Fork {forkLink}")
+        reposOut.append(html_details(f"{repoLink} - {description}",html_list(events)))
+    return "".join(reposOut)
+
+
 
 def section_community(user):
     print(html_header("Community",3))
     print(print_github_users("Followers",api_github(user['followers_url'])))
     print(print_github_users("Following",api_github(user['following_url'])))
 
-
 def section_activity(user):
-    print(print_recent_repositories(api_github(user['events_url'])))
+    events = api_github(user['events_url'] + "/public")
+    print(print_recent_repositories(events))
 
 # Where the magic happens. This will output the markdown contents
 if __name__ == "__main__":
     user = api_github("https://api.github.com/users/LittleTealeaf")
     print(print_json_file("LittleTealeaf.json",open_json("./assets/user.json")))
-    section_community(user)
+    # section_community(user)
     section_activity(user)
--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!/bin/env sh
-python3 ./python/readme.py $(cat $(ls | grep "github_token")) > README.md
+FILE="./github_token"
+if test -f "$FILE"; then
+    python3 ./python/readme.py $(cat $(ls | grep "github_token")) > README.md
+else
+    python3 ./python/readme.py $PERSONAL_ACCESS_TOKEN > README.md
+fi
