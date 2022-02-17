309a099b3836154bc3fcc1821f4faedb7e6a4891
Thomas Kwashnak <shintaomonk@gmail.com>
2022-02-16 19:16:39 -0500

    Reformatted Recent Activity

--- python/html.py
+++ python/html.py
@@ -8,10 +8,14 @@ def html_img(source,alt="",style=""):
     return f"<img src=\"{source}\" alt = \"{alt}\" style=\"{style}\">"
 
 def html_list(values,tag="ul"):
+    """
+    Creates an HTML List from contents
+    """
     string = "</li><li>".join(values)
     return f"<ul><li>{string}</li></ul>"
 
 def html_link(content,link):
+    
     return html_tag(f"a href=\"{link}\"",content)
 
 def html_line():
--- python/readme.py
+++ python/readme.py
@@ -31,76 +31,236 @@ def print_json_file(name,jsonObject):
     return f"{fileName}\n\n{jsonBlock}"
 
 def print_recent_repositories(events):
-    repos = {}
-    for event in events:
-        repo_name = event['repo']['name']
-        if repo_name not in repos:
-            repos[repo_name] = {
-                'api':api_github(event['repo']['url']),
-                'events': []
-            }
-        repo = repos[repo_name]
-        if event['type'] == 'CreateEvent':
-            type = event['payload']['ref_type'].capitalize()
-            repo['events'].append(f"{type} Created: <code>{event['payload']['ref']}</code>")
+    max_text_length = 50
 
-        elif event['type'] == 'DeleteEvent':
-            type = event['payload']['ref_type'].capitalize()
-            repo['events'].append(f"{type} Deleted: <code>{event['payload']['ref']}</code>")
-        elif event['type'] == 'ForkEvent':
-            forkLink = html_link(event['payload']['forkee']['full_name'],event['payload']['forkee']['html_url'])
-            repo['events'].append(f"Created Fork {forkLink}")
-        elif event['type'] == 'GollumEvent':
-            repo['events'].append("Gollum Event")
-        elif event['type'] == 'IssueCommentEvent':
-            repo['events'].append("Issue Comment Event")
-        elif event['type'] == 'IssuesEvent':
-            issue = event['payload']['issue']
-            issueLink = html_link(f"#{issue['number']}",issue['html_url'])
-            if event['payload']['action'] in ['opened', 'closed', 'reopened']:
-                verb = event['payload']['action'].capitalize()
-                repo['events'].append(f"Issue <code>{issueLink}</code> {verb} - {issue['title']}")
-            elif event['payload']['action'] == 'edited':
-                repo['events'].append('Issue Edited')
-            elif event['payload']['action'] == 'assigned':
-                repo['events'].append('Issue Assigned')
-            elif event['payload']['action'] == 'unassigned':
-                repo['events'].append('Issue Unassigned')
-            elif event['payload']['action'] == 'labeled':
-                repo['events'].append('Issue Labeled')
-            elif event['payload']['action'] == 'unlabeled':
-                repo['events'].append('Issue Unlabeled')
-        elif event['type'] == 'MemeberEvent':
-            repo['events'].append("Member Event")
-        elif event['type'] == 'PublicEvent':
-            repo['events'].append("Repository is now Public")
-        elif event['type'] == 'PullRequestEvent':
-            repo['events'].append("Pull Request Event")
-        elif event['type'] == 'PullRequestReviewEvent':
-            repo['events'].append("Pull Request Review Event")
-        elif event['type'] == 'PullRequestReviewCommentEvent':
-            repo['events'].append("Pull Request Review Comment Event")
-        elif event['type'] == 'PushEvent':
-            branch = event['payload']['ref'].split('/')[2]
-            for commit in event['payload']['commits']:
-                sha = commit['sha'][:7].upper()
+    def format_message(message):
+        msg = message.partition('\n')[0].split(' ')
+        count = 0
+        string = []
+        while count < max_text_length and len(msg) > 0:
+            s = msg.pop(0)
+            count += len(s)
+            string.append(s)
+        if len(msg) > 0:
+            return ' '.join(string) + "..."
+        else:
+            return ' '.join(string)
+
+    def format_event(event):
+        t = event['type']
+        payload = event['payload']
+        if t == 'CommitCommentEvent':
+
+            message = format_message(payload['comment']['body'].partition('\n')[0])
+            commitLink = html_link(f"#{payload['comment']['commit_id'][:7]}",payload['comment']['html_url'])
+
+            return f"Commented on commit {commitLink}: \"{message}\""
+
+        elif t in ['CreateEvent', 'DeleteEvent']:
+
+            verb = event['type'].replace('Event','d')
+            createType = payload['ref_type'].capitalize()
+            return f"{verb} {createType}: {payload['ref']}"
+        
+        elif t == 'ForkEvent':
+
+            forkLink = html_link(payload['forkee']['name'],payload['forkee']['html_url'])
+            return f"Created Fork {forkLink}"
+
+        elif t == 'GollumEvent':
+
+            pages = ", ".join([html_link(page['page_name'],page['html_url']) for page in payload['pages']])
+            return f"Updated Wiki Pages: {pages}"
+
+        elif t == 'IssueCommentEvent':
+            issueLink = html_link(f"#{payload['issue']['number']} {payload['issue']['title']}",payload['comment']['html_url'])
+
+            if payload['action'] == 'created':
+                return f"Commented on issue {issueLink}"
+
+            elif payload['action'] == 'edited':
+                return f"Edited comment on issue {issueLink}"
+            
+            elif payload['action'] == 'deleted':
+                issueLink = html_link(f"#{payload['issue']['number']}",payload['issue']['html_url'])
+                return f"Deleted comment on issue {issueLink}"
+
+        elif t == 'IssuesEvent':
+            a = payload['action']
+            issueLink = html_link(f"#{payload['issue']['id']}",payload['issue']['html_url'])
+
+            if a in ['opened', 'edited', 'closed', 'reopened']:
+                verb = a.capitalize()
+                return f"{verb} issue {issueLink}"
+
+            elif a in ['labeled', 'unlabeled']:
+                verb = 'Added'
+                if a == 'unlabeled':
+                    verb = 'Removed'
+                return f"{verb} Label <code>{payload['label']}</code> to issue {issueLink}"
+            elif a in ['assigned','unassigned']:
+
+                verb = 'Added'
+                if a == 'unassigned':
+                    verb = 'Removed'
+                assigneeLink = html_link(payload['assignee']['login'],payload['assignee']['html_url'])
+                return f"{verb} {assigneeLink} to issue {issueLink}"
+        
+        elif t == 'MemberEvent':
+            return None # TODO: Figure this out?
+
+        elif t == 'PublicEvent':
+            return "Repository is now public!"
+        
+        elif t == 'PullRequestEvent':
+            a = payload['action']
+            prLink = html_link(f"#{payload['pull_request']['number']} {payload['pull_request']['title']}",payload['pull_request']['html_url'])
+
+            if a in ['opened', 'edited', 'closed', 'reopened', 'synchronized']:
+                verb = a.capitalize()
+                return f"{verb} Pull Request {prLink}"
+        
+        elif t == 'PullRequestReviewEvent':
+            a = payload['review']['event']
+            prLink = html_link(f"#{payload['pull_request']['number']} {payload['pull_request']['title']}",payload['pull_request']['html_url'])
+
+            if a == 'APPROVE':
+                return f"Reviewed Pull Request {prLink}: Approved"
+            elif a == 'REQUEST_CHANGES':
+                return f"Reviewed Pull Request {prLink}: Requesting Changes"
+            elif a == 'COMMENT':
+                return f"Reviewed Pull Request {prLink}: Added Comment"
+            # elif a == 'PENDING':
+            #     return f"Reviewed Pull Request {prLink}: Pending"
+        
+        elif t == 'PullRequestReviewCommentEvent':
+            return None # TODO figure out what this is
+        
+        elif t == 'PushEvent':
+            repoApi = api_github(event['repo']['url'])
+            repoLink = html_link(repoApi['name'],repoApi['html_url'])
+
+            branch = payload['ref'].split('/')[-1]
+
+            if payload['size'] > 1:
+                commits = []
+                for commit in payload['commits']:
+                    commitApi = api_github(commit['url'])
+                    commitLink = html_link(f"#{commitApi['sha'][:7]}",commitApi['html_url'])
+                    message = format_message(commitApi['commit']['message'])
+                    commits.append(f"{commitLink}: {message}")
+    
+               
+                
+                commitList = html_list(commits)
+
+                commitCount = len(commits)
+                return f"Pushed {commitCount} commits to <code>{branch}</code>{commitList}"
+            
+            else:
+                commit = payload['commits'][0]
+
                 commitApi = api_github(commit['url'])
-                commitLink = html_link(f"#{sha}",commitApi['html_url'])
-                message = commit['message'].partition('\n')[0]
-                repo['events'].append(f"Commit <code>{commitLink} {branch}</code> {message}")
-        elif event['type'] == 'ReleaseEvent':
-            repo['events'].append("Release Event")
-        elif event['type'] == 'SponsorshipEvent':
-            repo['events'].append("Sponsorship Event")
-        elif event['type'] == 'WatchEvent':
-            repo['events'].append("Started watching repository")
-    reposOut = []
-    for repo_name in repos:
-        repo = repos[repo_name]
-        repoLink = html_link(repo['api']['full_name'],repo['api']['html_url'])
-        description = repo['api']['description']
-        reposOut.append(html_details(f"{repoLink} - {description}",html_list(repo['events'])))
-    return "".join(reposOut)
+                commitLink = html_link(f"#{commitApi['sha'][:7]}",commitApi['html_url'])
+                message = format_message(commitApi['commit']['message'])
+
+                return f"Pushed to <code>{branch}</code> {commitLink}: {message}"
+        
+        elif t == 'ReleaseEvent':
+            a = payload['action']
+            releaseLink = html_link(payload['release']['name'],payload['release']['html_url'])
+
+            if a == 'published':    
+                return f"Released {releaseLink}"
+            elif a == 'edited':
+                return f"Modified {releaseLink}"
+
+        return None
+        
+
+    printEvents = {}
+    for event in events:
+        if event['repo']['name'] not in printEvents:
+            formattedEvent = format_event(event)
+            if formattedEvent:
+                repoApi = api_github(event['repo']['url'])
+                repoLink = html_link(event['repo']['name'],repoApi['html_url'])
+                repoDescription = repoApi['description']
+                if repoDescription == 'None':
+                    repoDescription = 'No Description'
+                printEvents[event['repo']['name']] = f"{repoLink} - {repoDescription}<br>{formattedEvent}"
+
+    repos = [printEvents[repo] for repo in printEvents]
+    return html_list(repos)
+
+    # repos = {}
+    # for event in events:
+    #     repo_name = event['repo']['name']
+    #     if repo_name not in repos:
+    #         repos[repo_name] = {'api':api_github(event['repo']['url']), 'events': []}
+    #     repo = repos[repo_name]
+    #     if event['type'] in ['CreateEvent', 'DeleteEvent']:
+    #         change = event['type'].replace('Event','') + 'd'
+    #         eventType = event['payload']['ref_type'].capitalize()
+    #         if eventType in ['Tag', 'Branch']:
+    #             repo['events'].append(f"{eventType} {change}: <code>{event['payload']['ref']}</code>")
+    #     elif event['type'] == 'ForkEvent':
+    #         forkLink = html_link(event['payload']['forkee']['full_name'],event['payload']['forkee']['html_url'])
+    #         repo['events'].append(f"Created Fork {forkLink}")
+    #     elif event['type'] == 'GollumEvent':
+    #         repo['events'].append("Gollum Event")
+    #     elif event['type'] == 'IssueCommentEvent':
+    #         if event['payload']['action'] == 'created':
+    #             ...
+    #         elif event['payload']['action'] == 'edited':
+    #             ...
+    #         elif event['payload']['action'] == 'deleted':
+    #             ...
+    #     elif event['type'] == 'IssuesEvent':
+    #         issue = event['payload']['issue']
+    #         issueLink = html_link(f"#{issue['number']}",issue['html_url'])
+    #         if event['payload']['action'] in ['opened', 'closed', 'reopened', 'edited']:
+    #             verb = event['payload']['action'].capitalize()
+    #             repo['events'].append(f"Issue <code>{issueLink}</code> {verb} - {issue['title']}")
+    #         elif event['payload']['action'] == 'assigned':
+    #             repo['events'].append('Issue Assigned')
+    #         elif event['payload']['action'] == 'unassigned':
+    #             repo['events'].append('Issue Unassigned')
+    #         elif event['payload']['action'] == 'labeled':
+    #             repo['events'].append('Issue Labeled')
+    #         elif event['payload']['action'] == 'unlabeled':
+    #             repo['events'].append('Issue Unlabeled')
+    #     elif event['type'] == 'MemeberEvent':
+    #         repo['events'].append("Member Event")
+    #     elif event['type'] == 'PublicEvent':
+    #         repo['events'].append("Repository is now Public")
+    #     elif event['type'] == 'PullRequestEvent':
+    #         repo['events'].append("Pull Request Event")
+    #     elif event['type'] == 'PullRequestReviewEvent':
+    #         repo['events'].append("Pull Request Review Event")
+    #     elif event['type'] == 'PullRequestReviewCommentEvent':
+    #         repo['events'].append("Pull Request Review Comment Event")
+    #     elif event['type'] == 'PushEvent':
+    #         branch = event['payload']['ref'].split('/')[2]
+    #         for commit in event['payload']['commits']:
+    #             sha = commit['sha'][:7].upper()
+    #             commitApi = api_github(commit['url'])
+    #             commitLink = html_link(f"#{sha}",commitApi['html_url'])
+    #             message = commit['message'].partition('\n')[0]
+    #             repo['events'].append(f"Commit <code>{commitLink} {branch}</code> {message}")
+    #     elif event['type'] == 'ReleaseEvent':
+    #         repo['events'].append("Release Event")
+    #     elif event['type'] == 'SponsorshipEvent':
+    #         repo['events'].append("Sponsorship Event")
+    #     elif event['type'] == 'WatchEvent':
+    #         repo['events'].append("Started watching repository")
+    # reposOut = []
+    # for repo_name in repos:
+    #     repo = repos[repo_name]
+    #     repoLink = html_link(repo['api']['full_name'],repo['api']['html_url'])
+    #     description = repo['api']['description']
+    #     reposOut.append(html_details(f"{repoLink} - {description}",html_list(repo['events'])))
+    # return "".join(reposOut)
 
 
 
