24c2ba8f49033abed0131b35c04a03300ac98ce4
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-05-22 19:10:38 -0400

    Changes

--- script.py
+++ script.py
@@ -2,4 +2,17 @@ import libs.cache as cache
 import libs.github as github
 
 
-github.getAPI("https://api.github.com/users/LittleTealeaf")
+content = ""
+def write(c: str):
+    global content
+    content = content + c + "\n"
+
+
+
+
+
+
+
+
+with open("./README.md",'w') as file:
+    file.write(content)
