5ee5dfb83794dec8cb889d89701121c9974a2750
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-08-14 13:02:40 -0400

    Authentication change

--- libs/wakatime.py
+++ libs/wakatime.py
@@ -2,29 +2,33 @@ import requests
 import os
 from dotenv import load_dotenv
 import libs.cache as cache
+import base64
 
 
 load_dotenv()
 
+
 def getData(endpoint: str, params: dict = {}):
 
-    key =f'WAKATIME - {endpoint}{params}'
+    key = f"WAKATIME - {endpoint}{params}"
     che = cache.get_cache(key)
     if che != None:
         return che
-
-        print
-
     params = params.copy()
-    params['api_key'] = os.getenv('WAKA_TOKEN')
 
-    url = f'https://www.wakatime.com{endpoint}'
-    response = requests.get(url,params=params)
+    url = f"https://www.wakatime.com{endpoint}"
+    response = requests.get(
+        url,
+        params=params,
+        headers={
+            "Authorization": f'Basic {base64.b64encode(os.getenv("WAKA_TOKEN").encode("ascii"))}'
+        },
+    )
     print(endpoint)
 
     if response:
         data = response.json()
 
-        cache.store_cache(key,data)
+        cache.store_cache(key, data)
         return data
     print(response.text)
